d064d727be85cfec3a2f1d5698f20cab87190b25
smitoi <stefan.mitoi@yahoo.ro>
2021-02-03 13:14:48 +0200

    Docs fixes

--- 4,9,12/R/cerinta4.r
+++ 4,9,12/R/cerinta4.r
@@ -3,8 +3,7 @@
 #' This function calculates the inverse of a function in a certain point.
 #' @param f(x) A function.
 #' @param x The point in which the inverse function will be calculated (will raise error if the inverse cannot be calculated in that point).
-#' @param lower The lower part of the codomain.
-#' @param upper The upper part of the codomain.
+#' @param lower,upper The codomain's range.
 #' @return Dataframe containing the root of f(x) - u.
 #' @keywords inverse inv function
 #' @export
@@ -22,24 +21,28 @@ inverse <- function (f, u, lower = -100, upper = 100) {
 
 #' Plot PDF
 #'
-#' This functions plots the PDF for a random discrete variable.
-#' @param x_min The lower part of the PDF's domain.
-#' @param x_max The upper part of the PDF's domain.
+#' This functions plots the PDF for a continuous random variable.
+#' @param f The PDF for a continuous random variable.
+#' @param x_min,x_max The range over which the PDF will be plotted.
 #' @keywords plot pdf plotpdf probability density function
+#' @examples
+#' f <- function(x) (1/2 * sin (x)) * (0 < x & x <= pi)
+#' plotPDF(f, 0, pi)
 #' @export
-plotPDF <- function(x_min, x_max) {
+plotPDF <- function(f, x_min, x_max) {
   t <- seq(x_min, x_max, 0.0075)
   plot (t, f (t), col = 'red', main = 'Probability Density Function', xlab = 'x', ylab = 'y')
 }
 
 #' Integrate PDF
 #'
-#' This functions integrates the PDF. It can be used to calculate the CDF in a certain point.
+#' This functions integrates the PDF of a certain continuous random variable. It can be used to calculate the CDF for that variable in a certain point.
 #' @param X The point in which the integral will be calculated.
+#' @param f The PDF for a continuous random variable.
 #' @return The value of the integral calculated.
 #' @keywords integrate pdf probability density function cumulative distribution function
 #' @export
-integratePDF <- function (X) {
+integratePDF <- function (f, X) {
   tryCatch (
     expr = {
       integrate (f, 0, X)$value
@@ -54,15 +57,18 @@ integratePDF <- function (X) {
 #' Plot CDF
 #'
 #' This functions plots the CDF for a random discrete variable.
-#' @param x_min The minimum point of the range over which the function will be plotted.
-#' @param x_max The maximum point of the range over which the function will be plotted.
+#' @param x_min,x_max The range over which the CDF will be plotted.
+#' @param f The PDF for a continuous random variable.
 #' @keywords plot cdf plotcdf cumulative distribution function
+#' @examples
+#' f <- function(x) (1/2 * sin (x)) * (0 < x & x <= pi)
+#' plotCDF(f, 0, pi)
 #' @export
-plotCDF <- function(x_min, x_max) {
+plotCDF <- function(f, x_min, x_max) {
   t <- seq(x_min, x_max, 0.0075)
   s <- c()
   for (v in t) {
-    s = append (s, integratePDF (v))
+    s = append (s, integratePDF (f, v))
   }
   plot (t, s, col = 'blue', main = 'Cumulative Distribution Function', xlab = 'x', ylab = 'y')
 }
--- 4,9,12/R/cerinta9.r
+++ 4,9,12/R/cerinta9.r
@@ -1,17 +1,20 @@
 #' Random Variable Generation Function
 #'
 #' This function calculates a set of n random variables using the inverse method.
+#' @param f The PDF used to generate n variables.
 #' @param n The number of the variables to be generated.
-#' @param x_min The lower part of the domain of the PDF of the variable.
-#' @param lower The upper part of the domain of the PDF of the variable.
+#' @param x_min,x_max The PDF's domain range.
 #' @return An array with n random generated values.
 #' @keywords random generation variable pdf
+#' @examples
+#' f <- function(x) (1/2 * sin (x)) * (0 < x & x <= pi)
+#' randomVariableGenerator (f, 100, 0, pi)
 #' @export
-randomVariableGenerator <- function(n, x_min, x_max) {
+randomVariableGenerator <- function(f, n, x_min, x_max) {
   u <- runif(n, min = 0, max = 1)
   r <- c()
   for (v in u) {
-    r <- append (r, inverse(integratePDF, v, x_min, x_max)[1]$root)
+    r <- append (r, inverse(function (x) (integratePDF(f, x)), v, x_min, x_max)[1]$root)
   }
   r
 }
